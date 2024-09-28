5e31653bdb37a02c37781b80663b1c89c7c4d4d2
Viktor9125 <152290176+Viktor9125@users.noreply.github.com>
2024-09-28 11:30:13 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,54 @@
+import telebot
+import parser_functions as ps
+
+TOKEN = '7895950057:AAH_U_S253n8ZCqxQwWhq4kQodC-CcdHtNM'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Добро пожаловать в бота для новостей!")
+
+
+@bot.message_handler(commands=['articles_by_flows'])
+def get_flow_for_article(message):
+    bot.send_message(message.chat.id, 'Напиши поток.')
+    bot.register_next_step_handler_by_chat_id(message.chat.id, get_articles_by_flows)
+
+
+def get_articles_by_flows(message):
+    flow = message.text.lower()
+    flows_names = ['develop', 'admin', 'design', 'management', 'marketing', 'popsci']
+    flows = {'разработка': flows_names[0],
+             'администрирование': flows_names[1],
+             'дизайн': flows_names[2],
+             'менеджмент': flows_names[3],
+             'маркетинг': flows_names[4],
+             'научпоп': flows_names[5]}
+    if flow in flows.keys():
+        articles = ps.articles_by_flows(flows.get(flow))
+        for article in articles:
+            bot.send_message(message.chat.id, f'Название: {article}. Сылка: {articles[article]}.')
+    else:
+        bot.send_message(message.chat.id, f'Поток {flow} не найден.')
+
+
+@bot.message_handler(commands=['articles'])
+def get_articles(message):
+    articles = ps.articles()
+    for article in articles:
+        bot.send_message(message.chat.id, f'Название: {article}. Сылка: {articles[article]}.')
+
+
+@bot.message_handler(commands=['complaint'])
+def get_text_for_complaint(message):
+    bot.send_message(message.chat.id, 'Введите текст.')
+    bot.register_next_step_handler_by_chat_id(message.chat.id, get_complaint)
+
+
+def get_complaint(message):
+    ps.complaint(message.chat.id, message.text)
+    bot.send_message(message.chat.id, 'Текст сохранен.')
+
+
+bot.polling()
---
+++ parser_functions.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def articles():
+    url = 'https://habr.com/ru/feed/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles_dict = {}
+    article_counter = 0
+    try:
+        all_hrefs_articles = soup.find_all('a', attrs={'class': "tm-title__link"})
+        for article in all_hrefs_articles:
+            article_name = article.find('span').text
+            article_link = f'https://habr.com{article.get("href")}'
+            articles_dict[article_name] = article_link
+            article_counter += 1
+            if article_counter == 10:
+                break
+        return articles_dict
+    except Exception as e:
+        print(e)
+        return None
+
+
+def articles_by_flows(flow):
+    url = f'https://habr.com/ru/flows/{str(flow)}/articles/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    articles_dict = {}
+    article_counter = 0
+    try:
+        all_hrefs_articles = soup.find_all('a', attrs={'class': "tm-title__link"})
+        for article in all_hrefs_articles:
+            article_name = article.find('span').text
+            article_link = f'https://habr.com{article.get("href")}'
+            articles_dict[article_name] = article_link
+            article_counter += 1
+            if article_counter == 10:
+                break
+        return articles_dict
+    except Exception as e:
+        print(e)
+        return None
+
+
+def complaint(client, text):
+    with open('complaints.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    client_data = data.get(str(client), {})
+
+    client_data[len(client_data) + 1] = str(text)
+
+    data[str(client)] = client_data
+
+    with open('complaints.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    articles_by_flows('admin')
